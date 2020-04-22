bf31a7b3150fc2b61dca9df13dbce5c0a86ae79d
emery <emery@localhost.localdomain>
2020-04-22 22:24:13 +0200

    init

---
+++ src/test/java/com/openshift/cloudnative/poc/autoscaling/facade/FacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openshift.cloudnative.poc.autoscaling.facade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
