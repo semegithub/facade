2e69a1e94ec4a4bfa3edc88de5b21d6106bd7b84
emery <emery@localhost.localdomain>
2020-04-24 22:17:16 +0200

    naming

--- src/main/java/com/openshift/cloudnative/poc/autoscaling/facade/ScalingController.java
+++ src/main/java/com/openshift/cloudnative/poc/autoscaling/facade/ScalingController.java
@@ -1,6 +1,7 @@
 package com.openshift.cloudnative.poc.autoscaling.facade;
 
 import java.security.SecureRandom;
+import java.util.Optional;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
@@ -8,7 +9,9 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -16,7 +19,7 @@ import io.swagger.annotations.ApiOperation;
 public class ScalingController {
 
 	@GetMapping(path = "/", produces = "text/html")
-	@ApiOperation("Api status")
+	@ApiOperation("API status")
 	public String status() {
 		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
 		String message = "Facade ready on host " + hostname + "\n";
@@ -25,62 +28,71 @@ public class ScalingController {
 
 		return message;
 	}
-	
-	@GetMapping(path = "/lightCPUcall", produces = "text/html")
-	@ApiOperation("Light CPU call")
-	public String lightCPUcall() {
-		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
-		String message = "Facade on host " + hostname + "\n";
 
+	@GetMapping(path = "/noCPUcall", produces = "text/html")
+	@ApiOperation("Direct API call")
+	public String noCPUcall() {
+		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+		String message = "Facade on host " + hostname;
+		long timer = System.currentTimeMillis();
+		message += " done in " + (System.currentTimeMillis() - timer) + "[ms]";
 		System.out.println(message);
 
 		return message;
 	}
 
-//	@GetMapping(path = "/facadelightredirect", produces = "application/text")
-//	public String lightredirect() {
-//		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
-//		String message = "Facade on host " + hostname + " - light redirect ";
-//
-//		RestTemplate restTemplate = new RestTemplate();
-//		String result = restTemplate.getForObject("http://localhost:8080/child/init", String.class);
-//		message += result;
-//
-//		System.out.println(message);
-//		return message;
-//	}
-
-	@GetMapping(path = "/highCPUcall", produces = "text/html")
-	@ApiOperation("Heavy CPU call")
-	public String highCPUcall() {
+	@GetMapping(path = "/highCPURedirectCall", produces = "text/html")
+	public String highCPURedirectCall(
+			@RequestParam(value = "loopNumber", defaultValue = "1000") Integer loopNumber,
+			@RequestParam(value = "childLoopNumber", defaultValue = "1000") Integer childLoopNumber) {
 		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
-		String message = "Facade on host " + hostname + " - delayed redirect ";
-		for (int i = 0; i < 1000000; i++) {
+		String message = "Facade on host " + hostname + " - high CPU API redirect  ";
+
+		long timer = System.currentTimeMillis();
+		generateCPU(loopNumber);
+		message += " done in " + (System.currentTimeMillis() - timer) + "[ms]";
+
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(
+				"http://localhost:8080/child/highCPUCall?childLoopNumber={" + childLoopNumber + "}", String.class);
+		message += result;
+
+		System.out.println(message);
+		return message;
+	}
+
+	@GetMapping(path = "/highCPUCall", produces = "text/html")
+	@ApiOperation(value = "Heavy CPU API call", notes = "Generate CPU by looping on cipher.update(), default value for the number of loops is 1000.")
+	public String highCPUcall(
+			@RequestParam(value = "loopNumber", required = false, defaultValue = "1000") Integer loopNumber) {
+		String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+		String message = "Facade on host " + hostname + " - high CPU API call ";
+		long timer = System.currentTimeMillis();
+		generateCPU(loopNumber);
+		message += " done in " + (System.currentTimeMillis() - timer) + "[ms]";
+		System.out.println(message);
+		return message;
+	}
+
+	private void generateCPU(Integer loopNumber) {
+		for (int i = 0; i < loopNumber; i++) {
 			try {
 				byte[] iv = new byte[16];
 				new SecureRandom().nextBytes(iv);
-
 				// IV
 				IvParameterSpec ivSpec = new IvParameterSpec(iv);
-
 				// Key
 				KeyGenerator generator = KeyGenerator.getInstance("AES");
 				generator.init(128);
 				SecretKey secretKey = generator.generateKey();
-
 				// Encrypt
 				Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
 				cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivSpec);
 				cipher.update("0123456789012345".getBytes());
-
-				byte[] data = cipher.doFinal();
+				cipher.doFinal();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-
-		System.out.println(message);
-
-		return message;
 	}
 }
