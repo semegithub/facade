56711e5c25338ac5b3472dff02c47c1b6b6b9b0d
emery <emery@localhost.localdomain>
2020-04-26 00:29:51 +0200

    d

--- src/main/java/com/openshift/cloudnative/poc/autoscaling/facade/ScalingController.java
+++ src/main/java/com/openshift/cloudnative/poc/autoscaling/facade/ScalingController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import io.swagger.annotations.ApiOperation;
 
